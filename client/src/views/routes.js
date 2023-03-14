df8580ab565f3d73243ffb530e0bdfea5e8a1e36
HasanovH93 <hasanovh14@gmail.com>
2023-03-14 11:33:28 +0100

    add basic MUI template

---
+++ client/src/views/routes.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+// Demo E-commerce
+import {
+  IndexView,
+  Cart as CartView,
+  Checkout as CheckoutView,
+  EmptyCart as EmptyCartView,
+  Listing as ListingView,
+  Promotions as PromotionsView,
+  OrderComplete as OrderCompleteView,
+  ProductOverview as ProductOverviewView,
+} from 'views';
+import SignupSimple from './SignupSimple';
+
+const routes = [
+  {
+    path: '/',
+    renderer: (params = {}) => <IndexView {...params} />,
+  },
+  {
+    path: '/cart',
+    renderer: (params = {}) => <CartView {...params} />,
+  },
+  {
+    path: '/checkout',
+    renderer: (params = {}) => <CheckoutView {...params} />,
+  },
+  {
+    path: '/empty-cart',
+    renderer: (params = {}) => <EmptyCartView {...params} />,
+  },
+  {
+    path: '/listing',
+    renderer: (params = {}) => <ListingView {...params} />,
+  },
+  {
+    path: '/promotions',
+    renderer: (params = {}) => <PromotionsView {...params} />,
+  },
+  {
+    path: '/order-complete',
+    renderer: (params = {}) => <OrderCompleteView {...params} />,
+  },
+  {
+    path: '/product-overview',
+    renderer: (params = {}) => <ProductOverviewView {...params} />,
+  },
+  {
+    path: '/signup',
+    renderer: (params = {}) =>  <SignupSimple {...params}/>,
+  }
+];
+
+export default routes;
