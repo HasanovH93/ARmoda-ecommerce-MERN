92ae687042cb3ebbaac013b1179217bda0b9e15c
HasanovH93 <hasanovh14@gmail.com>
2023-04-15 18:47:00 +0200

    move the form in addProduct component

--- client/src/admin/pages/AddProduct/AddProduct.js
+++ client/src/admin/pages/AddProduct/AddProduct.js
@@ -1,27 +1,136 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import ProductForm from "./ProductForm/ProductForm";
 import styles from "./AddProduct.module.scss";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, Container, Button } from "react-bootstrap";
+import ImageDropzone from "./ImageDropzone/ImageDropzone";
+import SelectionForm from "./SelectionForm/SelectionForm";
+import VariationForm from "./VariationForm/VariationForm";
+import { setProducts } from "../../../store/slices/product-slice";
+import api from "../../../api";
+import { categoriesList, tagsList } from "../../data/data";
 
 const AddProduct = () => {
   const sidebarOpen = useSelector((state) => state.sidebar.sidebarOpen);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [product, setProduct] = useState({
+    productName: "",
+    description: "",
+    price: "",
+    discount: "",
+    files: [],
+    variations: [],
+    categories: [],
+    tags: [],
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    const completeProduct = { ...product, variations, categories, tags };
+
+    Object.entries(completeProduct).forEach(([key, value]) => {
+      if (key === "files") {
+        value.forEach((file) => {
+          formData.append("img", file);
+        });
+      } else if (
+        key === "variations" ||
+        key === "categories" ||
+        key === "tags"
+      ) {
+        formData.append(key, JSON.stringify(value));
+      } else {
+        formData.append(key, value);
+      }
+    });
+
+    try {
+      console.log(formData);
+      const response = await api.post("/hotels/create", formData);
+      dispatch(setProducts(response.data.createdData));
+      navigate("/dashboard/all-products");
+    } catch (error) {
+      console.error("Error while adding hotel:", error);
+    }
+  };
+
+  const [variations, setVariations] = useState([{ color: "", size: [] }]);
+
+  const [categories, setCategories] = useState([]);
+  const [tags, setTags] = useState([]);
 
   console.log("Add-Products", sidebarOpen);
   return (
     <div
       className={`${styles.container} ${sidebarOpen ? "" : styles.sidebarOpen}`}
     >
-      <Row>
-        <Col>
-          <h2 className={styles.title}>Add Product</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <ProductForm />
-        </Col>
-      </Row>
+      <Container>
+        <Form onSubmit={handleSubmit} className={styles.createForm}>
+          <Row>
+            <Col>
+              <h2 className={styles.title}>Add Product</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <ProductForm product={product} setProduct={setProduct} />
+
+              <Form.Group controlId="image">
+                <Form.Label>Product Image:</Form.Label>
+                <div className="image-dropzone-container">
+                  <ImageDropzone product={product} setProduct={setProduct} />
+                </div>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="variationsAndCategories">
+                <Row>
+                  <Col sm={12} md={6}>
+                    <Form.Label>Product Variations:</Form.Label>
+                    <VariationForm
+                      variations={variations}
+                      setVariations={setVariations}
+                    />
+                  </Col>
+                  <Col sm={12} md={3}>
+                    <SelectionForm
+                      title="Categories"
+                      items={categoriesList}
+                      selectedItems={categories}
+                      setSelectedItems={setCategories}
+                    />
+                  </Col>
+                  <Col sm={12} md={3}>
+                    <SelectionForm
+                      title="Tags"
+                      items={tagsList}
+                      selectedItems={tags}
+                      setSelectedItems={setTags}
+                    />
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Col>
+          </Row>
+          <div className={styles.centerButton}>
+            <Button
+              variant="primary"
+              type="submit"
+              className={`${styles.addButton}`}
+            >
+              Add Product
+            </Button>
+          </div>
+        </Form>
+      </Container>
     </div>
   );
 };
--- client/src/admin/pages/AddProduct/ProductForm/ProductForm.js
+++ client/src/admin/pages/AddProduct/ProductForm/ProductForm.js
@@ -1,31 +1,8 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
-import { Form, Button, Row, Col, Container } from "react-bootstrap";
-import ImageDropzone from "../ImageDropzone/ImageDropzone";
-import api from "../../../../api";
-import VariationForm from "../VariationForm/VariationForm";
-import { setProducts } from "../../../../store/slices/product-slice";
+import { Form, Row, Col, Container } from "react-bootstrap";
 import styles from "./ProductForm.module.scss";
-import SelectionForm from "../SelectionForm/SelectionForm";
-import { categoriesList, tagsList } from "../../../data/data";
 import useHandleInputChange from "../../../hooks/HandleInputChange";
 
-const ProductForm = () => {
-  const [product, setProduct] = useState({
-    productName: "",
-    description: "",
-    price: "",
-    discount: "",
-    files: [],
-    variations: [],
-    categories: [],
-    tags: [],
-  });
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-
+const ProductForm = ({ product, setProduct }) => {
   const handleInputChange = useHandleInputChange(
     product,
     setProduct,
@@ -36,149 +13,62 @@ const ProductForm = () => {
     null
   );
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-    const completeProduct = { ...product, variations, categories, tags };
-
-    Object.entries(completeProduct).forEach(([key, value]) => {
-      if (key === "files") {
-        value.forEach((file) => {
-          formData.append("img", file);
-        });
-      } else if (
-        key === "variations" ||
-        key === "categories" ||
-        key === "tags"
-      ) {
-        formData.append(key, JSON.stringify(value));
-      } else {
-        formData.append(key, value);
-      }
-    });
-
-    try {
-      console.log(formData);
-      const response = await api.post("/hotels/create", formData);
-      dispatch(setProducts(response.data.createdData));
-      navigate("/dashboard/all-products");
-    } catch (error) {
-      console.error("Error while adding hotel:", error);
-    }
-  };
-
-  const [variations, setVariations] = useState([{ color: "", size: [] }]);
-
-  const [categories, setCategories] = useState([]);
-  const [tags, setTags] = useState([]);
-
   return (
     <Container>
-      <Form onSubmit={handleSubmit} className={styles.createForm}>
-        <Row>
-          <Row>
-            <Col sm={12} md={4}>
-              <Form.Group controlId="productName">
-                <Form.Label className={styles.label}>Product Name:</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="productName"
-                  value={product.productName}
-                  onChange={(event) => handleInputChange("product", event)}
-                  required
-                />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={8}>
-              <Form.Group controlId="description">
-                <Form.Label className={styles.label}>Description:</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="description"
-                  value={product.description}
-                  onChange={(event) => handleInputChange("product", event)}
-                  required
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-        </Row>
-
-        <Row>
-          <Col sm={12} md={2}>
-            <Form.Group controlId="price">
-              <Form.Label>Price:</Form.Label>
-              <Form.Control
-                type="number"
-                step="0.01"
-                name="price"
-                value={product.price}
-                onChange={(event) => handleInputChange("product", event)}
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col sm={12} md={2}>
-            <Form.Group controlId="discount">
-              <Form.Label>Discount:</Form.Label>
-              <Form.Control
-                type="number"
-                step="0.01"
-                name="discount"
-                value={product.discount}
-                onChange={(event) => handleInputChange("product", event)}
-                required
-              />
-            </Form.Group>
-          </Col>
-          <Col sm={12} md={8}>
-            <Form.Group controlId="image">
-              <Form.Label>Product Image:</Form.Label>
-              <div className="image-dropzone-container">
-                <ImageDropzone product={product} setProduct={setProduct} />
-              </div>
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Form.Group controlId="variationsAndCategories">
-          <Row>
-            <Col sm={12} md={6}>
-              <Form.Label>Product Variations:</Form.Label>
-              <VariationForm
-                variations={variations}
-                setVariations={setVariations}
-              />
-            </Col>
-            <Col sm={12} md={3}>
-              <SelectionForm
-                title="Categories"
-                items={categoriesList}
-                selectedItems={categories}
-                setSelectedItems={setCategories}
-              />
-            </Col>
-            <Col sm={12} md={3}>
-              <SelectionForm
-                title="Tags"
-                items={tagsList}
-                selectedItems={tags}
-                setSelectedItems={setTags}
-              />
-            </Col>
-          </Row>
-        </Form.Group>
-        <div className={styles.centerButton}>
-          <Button
-            variant="primary"
-            type="submit"
-            className={`${styles.addButton}`}
-          >
-            Add Product
-          </Button>
-        </div>
-      </Form>
+      <Row>
+        <Col sm={12} md={4}>
+          <Form.Group controlId="productName">
+            <Form.Label className={styles.label}>Product Name:</Form.Label>
+            <Form.Control
+              type="text"
+              name="productName"
+              value={product.productName}
+              onChange={(event) => handleInputChange("product", event)}
+              required
+            />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={8}>
+          <Form.Group controlId="description">
+            <Form.Label className={styles.label}>Description:</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="description"
+              value={product.description}
+              onChange={(event) => handleInputChange("product", event)}
+              required
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={4}>
+          <Form.Group controlId="price">
+            <Form.Label>Price:</Form.Label>
+            <Form.Control
+              type="number"
+              step="0.01"
+              name="price"
+              value={product.price}
+              onChange={(event) => handleInputChange("product", event)}
+              required
+            />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={4}>
+          <Form.Group controlId="discount">
+            <Form.Label>Discount:</Form.Label>
+            <Form.Control
+              type="number"
+              step="0.01"
+              name="discount"
+              value={product.discount}
+              onChange={(event) => handleInputChange("product", event)}
+              required
+            />
+          </Form.Group>
+        </Col>
+      </Row>
     </Container>
   );
 };
